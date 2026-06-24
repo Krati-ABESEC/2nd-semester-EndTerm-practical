5269c0a8feced736add18aea2ff9d2a2953bb2fb
Krati Agarwal <kratiagarwal7812@gmail.com>
2026-06-24 10:01:48 +0530

    EndTerm Practical code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let itext = document.getElementById("text");
-let n= itext.value;
 let res = document.getElementById("result");
 
 
 document.getElementById("check").addEventListener("click", function () {
-  if (n % 2 === 0) {
-    res.innerHTML = "The number is Even";
-  } else {
-    res.innerHTML= "The number is Odd";
-  }
+    let n= itext.value;
+    if (n % 2 === 0) {
+        res.innerHTML = "The number is Even";
+    } else {
+        res.innerHTML= "The number is Odd";
+    }
 });
 
 document.getElementById("reset").addEventListener("click", function () {
